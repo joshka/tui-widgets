78cd3ae1e1bd7a0ad8190025ceeb77202756cf45
Josh McKinney <joshka@users.noreply.github.com>
2023-12-30 19:21:57 -0800

    docs: add doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! A popup widget for [Ratatui](https://ratatui.rs)
 //!
+//! The popup widget is a simple widget that renders a popup in the center of the screen.
+//!
 //! # Example
 //!
 //! ```rust
@@ -21,19 +23,58 @@ use ratatui::{
     widgets::{Block, Borders, Clear, Paragraph, Widget},
 };
 
+/// Configuration for a popup.
+///
+/// This struct is used to configure a [`PopupWidget`]. It can be created using
+/// [`Popup::new`](Popup::new). Convert it into a Ratatui widget using
+/// [`Popup::to_widget`](Popup::to_widget).
+///
+/// # Example
+///
+/// ```rust
+/// use ratatui::prelude::*;
+/// use tui_popup::Popup;
+///
+/// fn render_popup(frame: &mut Frame) {
+///     let popup = Popup::new("tui-popup demo", "Press any key to exit")
+///         .style(Style::new().white().on_blue());
+///     frame.render_widget(popup.to_widget(), frame.size());
+/// }
+/// ```
 #[derive(Clone, Debug, Default, Setters)]
+#[setters(into)]
 pub struct Popup<'content> {
+    /// The title of the popup.
     pub title: Line<'content>,
+    /// The body of the popup.
     pub body: Text<'content>,
+    /// The style to apply to the entire popup.
     pub style: Style,
 }
 
+/// A Ratatui widget that renders a popup.
 #[derive(Clone, Debug)]
 pub struct PopupWidget<'content> {
     popup: &'content Popup<'content>,
 }
 
 impl<'content> Popup<'content> {
+    /// Create a new popup with the given title and body.
+    ///
+    /// # Parameters
+    ///
+    /// - `title` - The title of the popup. This can be any type that can be converted into a
+    ///   [`Line`].
+    /// - `body` - The body of the popup. This can be any type that can be converted into a
+    ///   [`Text`].
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tui_popup::Popup;
+    ///
+    /// let popup = Popup::new("tui-popup demo", "Press any key to exit");
+    /// ```
     pub fn new<L, T>(title: L, body: T) -> Self
     where
         L: Into<Line<'content>>,
@@ -46,6 +87,7 @@ impl<'content> Popup<'content> {
         }
     }
 
+    /// Convert the popup into a Ratatui widget.
     pub fn to_widget(&'content self) -> PopupWidget<'content> {
         PopupWidget { popup: self }
     }
@@ -68,6 +110,7 @@ impl Widget for PopupWidget<'_> {
     }
 }
 
+/// Create a rectangle centered in the given area.
 fn centered_rect(width: u16, height: u16, area: Rect) -> Rect {
     Rect {
         x: area.width.saturating_sub(width) / 2,
