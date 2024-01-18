cd3021e6709ee04314df57c2aba9afe57648d09c
Josh McKinney <joshka@users.noreply.github.com>
2024-01-17 21:46:23 -0800

    fix: fix bugs in doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -15,20 +15,21 @@
 //! ## Usage
 //!
 //! ```rust
-//! use tui_scrollview::ScrollView;
-//! use ratatui::{prelude::*, layout::Size};
+//! use std::iter;
+//! use tui_scrollview::{ScrollView, ScrollViewState};
+//! use ratatui::{layout::Size, prelude::*, widgets::*};
 //!
-//! fn render(Frame: &mut Frame) {
+//! fn render(frame: &mut Frame) {
 //!     let size = Size::new(10, 100);
 //!     let mut scroll_view = ScrollView::new(size);
 //!     let some_long_string =
 //!         iter::repeat("Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n")
 //!            .take(100)
 //!            .collect::<String>();
-//!     let area = size.into();
-//!     scroll_view.render_widget(Text::from(some_long_string), area);
-//!     let state = ScrollViewState::default();
-//!     frame.render_stateful_widget(scroll_view, area);
+//!     let area = Rect::new(0, 0, 10, 100);
+//!     scroll_view.render_widget(Paragraph::new(some_long_string), area);
+//!     let mut state = ScrollViewState::default();
+//!     frame.render_stateful_widget(scroll_view, area, &mut state);
 //! }
 //! ```
 //!
