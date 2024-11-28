b08264668efa98661c1271bb30ec9579cc666da3
Josh McKinney <joshka@users.noreply.github.com>
2024-11-27 20:48:32 -0800

    chore: test SizedWrapper, add setters and new()

--- tui-popup/src/sized_wrapper.rs
+++ tui-popup/src/sized_wrapper.rs
@@ -1,11 +1,17 @@
 use std::fmt::Debug;
 
+use derive_setters::Setters;
 use ratatui::{buffer::Buffer, layout::Rect, widgets::WidgetRef};
 
 use crate::SizedWidgetRef;
 
-#[derive(Debug)]
+/// The `SizedWrapper` struct wraps a widget and provides a fixed size for it.
+///
+/// This struct is used to wrap a widget and provide a fixed size for it. This is useful when you
+/// want to use a widget that does not implement `SizedWidgetRef` as the body of a popup.
+#[derive(Debug, Setters)]
 pub struct SizedWrapper<W> {
+    #[setters(skip)]
     pub inner: W,
     pub width: usize,
     pub height: usize,
@@ -24,5 +30,54 @@ impl<W: WidgetRef> SizedWidgetRef for SizedWrapper<W> {
 
     fn height(&self) -> usize {
         self.height
+    }
+}
+
+impl<W> SizedWrapper<W> {
+    /// Create a new `SizedWrapper` with the given widget and size.
+    pub fn new(inner: W, width: usize, height: usize) -> Self {
+        Self {
+            inner,
+            width,
+            height,
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use ratatui::{buffer::Buffer, layout::Rect};
+
+    use super::*;
+
+    struct TestWidget;
+
+    impl WidgetRef for TestWidget {
+        fn render_ref(&self, _area: Rect, _buf: &mut Buffer) {
+            "Hello".render_ref(_area, _buf);
+        }
+    }
+
+    #[test]
+    fn test_sized_wrapper_new() {
+        let widget = TestWidget;
+        let wrapper = SizedWrapper::new(widget, 10, 20);
+        assert_eq!(wrapper.width, 10);
+        assert_eq!(wrapper.height, 20);
+    }
+
+    #[test]
+    fn test_sized_wrapper_render() {
+        let widget = TestWidget;
+        let wrapper = SizedWrapper::new(widget, 20, 5);
+        let mut buffer = Buffer::empty(Rect::new(0, 0, 20, 5));
+        wrapper.render_ref(buffer.area, &mut buffer);
+        let expected = Buffer::with_lines([
+            "Hello               ",
+            "                    ",
+            "                    ",
+            "                    ",
+            "                    ",
+        ]);
+        assert_eq!(buffer, expected);
     }
 }
